a9f474f34bce8a4c4d471278ce5bca9ecfa95188
box <shusbox@naver.com>
2025-07-12 21:02:15 +0900

    내가유튜브api를해냄

---
+++ script/youtubedata.js
@@ -0,0 +1,23 @@
+const key = 'AIzaSyDmXIRUFlgllGyXwFNiEOlqLJV_GQFtUR0';
+const videoId = 'nz2_gNQDiVM';
+const endpoint = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${key}`;
+
+function formatViewCount_view(count) {
+  const num = Number(count);
+  return (num / 1_0000).toFixed(0).replace(/\.0$/, '');
+}
+
+function formatViewCount_like(count) {
+  const num = Number(count);
+  return (num / 1_0000).toFixed(1).replace(/\.0$/, '');
+}
+
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+    const stats = data.items[0].statistics;
+    document.getElementById('view-count').textContent = `${formatViewCount_view(stats.viewCount)}만`;
+    document.getElementById('like-count').textContent = `${formatViewCount_like(stats.likeCount)}만`;
+    document.getElementById('comment-count').textContent = `${Number(stats.commentCount)}`;
+  })
+  .catch(error => console.error('YouTube API 에러:', error));
